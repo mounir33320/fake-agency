6bb7f0d8f653990bcd1726e5b30768d98861f9d8
Mounir S <mounir.senaoui@gmail.com>
2021-02-15 20:38:24 +0100

    Modification des contraintes dans l'entite Property

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -116,14 +116,14 @@ class Property
     private $imageFile;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      *
      * @var string|null
      */
     private $imageName;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime", nullable=true)
      */
     private $updated_at;
 
